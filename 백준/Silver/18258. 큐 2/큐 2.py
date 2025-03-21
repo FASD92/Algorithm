faacef00d14e7973df4b1fdf61f3255d68986ff2
cassano <91974257+FASD92@users.noreply.github.com>
2025-03-21 11:41:29 +0900

    [Silver IV] Title: 큐 2, Time: 1816 ms, Memory: 256240 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,5 +1,19 @@
 """
+
+출력 입력할 빈 deque 선언
+리스트 컴프리헨션으로 테스트 케이스 T만큼 commands 리스트에 채우기
+
+함수 6개 선언
+삼항연산자 연습
 딕셔너리로 명령어 6개 저장
+
+반복문 range는 T
+만약 commands[i]가 push로 시작하면
+commands[i]를 공백으로 split하고 언패킹 하고
+두번째 항목을 int형으로 변환해서 push 함수를 실행
+
+그 외에는 딕셔너리에서 commands[i]의 문자열과 일치하는 함수를 실행하는데
+딱히 인자가 필요 없으니 인자는 비워서 ()으로 만든다
 """
 
 import sys
@@ -7,7 +21,7 @@ from collections import deque
 
 T = int(sys.stdin.readline().strip())
 result = deque()
-commands = [sys.stdin.readline().strip() for i in range(T)]
+commands = [sys.stdin.readline().strip() for _ in range(T)]
 
 def push(x):
     result.append(x)
