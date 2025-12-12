309bb807b84332a03f168cf62dd952ef468f4564
cassano <91974257+FASD92@users.noreply.github.com>
2025-12-13 04:52:36 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,10 @@
+def solution(a, b, c):
+    answer = 0
+    dice_set = {a, b, c}
+    if len(dice_set) == 3:
+        answer = a + b + c
+    elif len(dice_set) == 2:
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+    else:
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+    return answer
